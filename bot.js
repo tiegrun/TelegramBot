13c8aeae03830e408c611e7d9653982316eb3cb8
tiegrun <tiko3020@gmail.com>
2025-08-18 22:42:54 -0700

    update to fix

--- bot.js
+++ bot.js
@@ -28,11 +28,11 @@ const channelId = -1003010205363;
 const mainSiteUrl = "https://tieg.run";
 
 // JSONBin endpoint (replace BIN_ID with your bin ID)
-const JSONBIN_URL = `https://api.jsonbin.io/v3/b/${JSONBIN_ID}/latest`;
+const JSONBIN_URL = `https://api.jsonbin.io/v3/b/${process.env.JSONBIN_ID}/latest`;
 
 const fetchBlogPosts = async () => {
   try {
-    const response = await axios.get(JSONBIN_URL, {
+    const response = await axios.get(process.env.JSONBIN_ID, {
       headers: {
         'X-Master-Key': process.env.JSONBIN_KEY  // required for private bins
       }
